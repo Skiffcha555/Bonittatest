c57b42e4060d41f278557efdc1d432fe97b96806
Almishev Nurbol <almishev.01@gmail.com>
2022-03-25 03:28:58 +0600

    update

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -3,7 +3,19 @@ window.addEventListener('DOMContentLoaded', () => {
         hamburger = document.querySelector('.burger');
 
     hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('burger_active');
+        // hamburger.classList.toggle('burger_active');
         menu.classList.toggle('burger-menu_active');
     });
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    // const menu = document.querySelector('.burger-menu')
+    let close_icon = document.querySelector('.burger-menu')
+    let close = document.querySelector('.close-icon3')
+
+    close_icon.addEventListener('click', () => {
+        menu.classList.remove('burger-menu_active');
+    });
+
+    close.onclick = () => document.querySelector('.burger-menu').classList.remove('burger-menu_active')
 })
