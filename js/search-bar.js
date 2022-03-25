ebcd72a378dda1398b7b0575c13af38d3af7458e
Dastan Kudaibergen <dkudaybergen@gmail.com>
2022-03-26 02:40:45 +0600

    update

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,11 +1,24 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.search-bar')
-    let search_icon = document.querySelector('.search-icon')
+    let search_icon = document.querySelectorAll('.search-icon')
     let close = document.querySelector('.close-icon2')
 
-    search_icon.addEventListener('click', () => {
-        menu.classList.add('search-bar_active');
+    const menuMobile = document.querySelector('.search-bar.mobile')
+    let search_iconMobile = document.querySelectorAll('.search-icon-mobile')
+    let closeMobile = document.querySelector('.close-icon2-mobile')
+    
+    search_icon.forEach(($search_icon) => {
+        $search_icon.addEventListener('click', () => {
+            menu.classList.add('search-bar_active');
+        });
+    });
+
+    search_iconMobile.forEach(($search_iconMobile) => {
+        $search_iconMobile.addEventListener('click', () => {
+            menuMobile.classList.add('search-bar_active');
+        });
     });
 
     close.onclick = () => document.querySelector('.search-bar').classList.remove('search-bar_active')
+    closeMobile.onclick = () => document.querySelector('.search-bar.mobile').classList.remove('search-bar_active')
 })
